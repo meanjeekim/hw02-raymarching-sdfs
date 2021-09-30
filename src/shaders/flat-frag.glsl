bf25ad0fd15fa6452722fae74d4bb3870ec028ec
allison <am.kim327@gmail.com>
2021-09-29 21:15:28 -0400

    get ray meh

--- src/shaders/flat-frag.glsl
+++ src/shaders/flat-frag.glsl
@@ -8,6 +8,28 @@ uniform float u_Time;
 in vec2 fs_Pos;
 out vec4 out_Col;
 
+vec3 getDirection(vec2 uv) {
+  vec3 look = normalize(u_Ref - u_Eye);
+  vec3 right = normalize(cross(look, u_Up));
+  vec3 up = cross(right, look);
+
+  float aspect_ratio = u_Dimensions.x / u_Dimensions.y;
+  vec3 vertical = up * tan(1.0);
+  vec3 horizontal = right * aspect_ratio * tan(1.0);
+  vec3 screen_point = (look + uv.x * horizontal + uv.y * vertical);
+
+  return normalize(screen_point - u_Eye);
+}
+
 void main() {
-  out_Col = vec4(0.5 * (fs_Pos + vec2(1.0)), 0.5 * (sin(u_Time * 3.14159 * 0.01) + 1.0), 1.0);
+  vec2 uv = fs_Pos;
+  uv = uv * 2.0 - 1.0;
+  
+  vec3 dir = getDirection(uv);
+
+  vec3 color = 0.5 * (dir + vec3(1.0));
+
+  // out_Col = vec4(0.5 * (fs_Pos + vec2(1.0)), 0.5 * (sin(u_Time * 3.14159 * 0.01) + 1.0), 1.0);
+  out_Col = vec4(color, 1.0);
+  // out_Col = vec4(fs_Pos, 1., 1.);
 }
